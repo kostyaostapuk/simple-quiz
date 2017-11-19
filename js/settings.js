d182c177bb535d426493d2441686f21175d7f6fe
Kostya Ostapuk <ostapuk97@gmail.com>
2017-11-20 01:05:37 +0300

    fix answers

--- js/settings.js
+++ js/settings.js
@@ -30,64 +30,64 @@ $(function(){
   		],
   		type: "grid"
 		},
-		//Вопрос 2
-		{
-  		title: "2. Заголовок вопроса",
-  		answers: [
-				"Ответ №1 вопроса 2",
-			  "Ответ №2 вопроса 2",
-				"Ответ №3 вопроса 2",
-				"Ответ №4 вопроса 2"],
-  		img: [
-    		"img/typeProduct/question-2/1.jpg",
-    		"img/typeProduct/question-2/2.jpg",
-    		"img/typeProduct/question-2/3.jpg",
-    		"img/typeProduct/question-2/4.jpg"
-  		],
-  		type: "grid"
-		},
-		//Вопрос 3
-		{
-  		title: "3. Заголовок вопроса",
-  		answers: [
-				"Ответ №1 вопроса 3",
-			  "Ответ №2 вопроса 3",
-				"Ответ №3 вопроса 3",
-				"Ответ №4 вопроса 3"],
-  		img: [
-    		"img/typeProduct/question-3/1.jpg",
-    		"img/typeProduct/question-3/2.jpg",
-    		"img/typeProduct/question-3/3.jpg",
-    		"img/typeProduct/question-3/4.jpg"
-  		],
-  		type: "list"
-		},
-		//Вопрос 4
-		{
-  		title: "4. Заголовок вопроса",
-  		answers: [
-				"Ответ №1 вопроса 1",
-			  "Ответ №2 вопроса 1",
-				"Ответ №3 вопроса 1",
-				"Ответ №4 вопроса 1"],
-  		img: [
-    		"img/typeProduct/question-4/1.jpg",
-    		"img/typeProduct/question-4/2.jpg",
-    		"img/typeProduct/question-4/3.jpg",
-    		"img/typeProduct/question-4/4.jpg"
-  		],
-  		type: "list"
-		},
+		// //Вопрос 2
+		// {
+  	// 	title: "2. Заголовок вопроса",
+  	// 	answers: [
+		// 		"Ответ №1 вопроса 2",
+		// 	  "Ответ №2 вопроса 2",
+		// 		"Ответ №3 вопроса 2",
+		// 		"Ответ №4 вопроса 2"],
+  	// 	img: [
+    // 		"img/typeProduct/question-2/1.jpg",
+    // 		"img/typeProduct/question-2/2.jpg",
+    // 		"img/typeProduct/question-2/3.jpg",
+    // 		"img/typeProduct/question-2/4.jpg"
+  	// 	],
+  	// 	type: "grid"
+		// },
+		// //Вопрос 3
+		// {
+  	// 	title: "3. Заголовок вопроса",
+  	// 	answers: [
+		// 		"Ответ №1 вопроса 3",
+		// 	  "Ответ №2 вопроса 3",
+		// 		"Ответ №3 вопроса 3",
+		// 		"Ответ №4 вопроса 3"],
+  	// 	img: [
+    // 		"img/typeProduct/question-3/1.jpg",
+    // 		"img/typeProduct/question-3/2.jpg",
+    // 		"img/typeProduct/question-3/3.jpg",
+    // 		"img/typeProduct/question-3/4.jpg"
+  	// 	],
+  	// 	type: "list"
+		// },
+		// //Вопрос 4
+		// {
+  	// 	title: "4. Заголовок вопроса",
+  	// 	answers: [
+		// 		"Ответ №1 вопроса 1",
+		// 	  "Ответ №2 вопроса 1",
+		// 		"Ответ №3 вопроса 1",
+		// 		"Ответ №4 вопроса 1"],
+  	// 	img: [
+    // 		"img/typeProduct/question-4/1.jpg",
+    // 		"img/typeProduct/question-4/2.jpg",
+    // 		"img/typeProduct/question-4/3.jpg",
+    // 		"img/typeProduct/question-4/4.jpg"
+  	// 	],
+  	// 	type: "list"
+		// }
 	];
 
 	//Generated Values for Quiz
 	$.each(question, function(key, value){
-		qstnTitle.text(value.title);
+		console.log(value.title);
 		for (var i = 0; i < value.answers.length; i++) {
 			var sizeCol=0;
 			switch (counter()) {
 				case 1:
-					sizeCol=12;
+					sizeCol=3;
 				break;
 				case 2:
 					sizeCol=6;
@@ -96,7 +96,7 @@ $(function(){
 					sizeCol=4;
 				break;
 				case 4:
-					sizeCol=3;
+					sizeCol=4;
 				break;
 				case 5:
 					sizeCol=2;
@@ -105,10 +105,15 @@ $(function(){
 					sizeCol=2;
 				break;
 			}
-			var bodyAnswer="<div class='col-md-"+sizeCol+"'>"+
+
+			qstnTitle.text(value.title);
+			var bodyAnswer="<div class='col-md-"+sizeCol+" text-center'>"+
 			value.answers[i]+
-			"<img class='img-responsive' src="+value.img[i]+">"+
-			"</div>";
+			"<br><img class='img-responsive' src="+value.img[i]+"><br>"+
+			"<input name='answer' value='question"+i+"' type='radio'>"+
+			"<div class='row'><div class='col-md-12'>"+
+			"<div id='place-btn'></div>"+
+			"</div></div></div>";
 			answerBox.append(bodyAnswer);
 		}
 	});
@@ -122,5 +127,11 @@ $(function(){
 		return count;
 	}
 	qstnCounterBox.text(counter());
+
+	//Handler event click on ipnut
+	$("input").on("click", function(){
+		var bodyBtn="<button id='next' class='btn btn-success'>Следующий вопрос</button>";
+		$("#place-btn").html(bodyBtn);
+	});
 
 });

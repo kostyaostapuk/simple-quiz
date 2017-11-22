099ca6d2ecca1dc13d58e406f391fbfbb9d16489
Kostya Ostapuk <ostapuk97@gmail.com>
2017-11-22 20:56:20 +0300

    fix: show Button, list display, height progress-bar and etc.

--- js/settings.js
+++ js/settings.js
@@ -2,28 +2,30 @@ $(function() {
   var qstnTitle = $(".question-title");
 
   var answerBox = $(".answer-box");
+  var listAdvantageBox = $("#list-advantage");
   var progressBar = $(".progress-bar");
   var discountBox = $("#discount");
   var daysBox = $("#days");
   var suffixBox = $("#days-suffix");
   var discriptBox = $(".discription");
-  var listGroupBox = $(".list-group");
-	var templateTitleBox=$(".template-title");
-	var qstnStartBox=$("#question-start");
-	var qstnEndBox = $("#question-end");
-	var btnBox=$(".btn-box");
+  var templateTitleBox = $(".template-title");
+  var qstnStartBox = $("#question-start");
+  var qstnEndBox = $("#question-end");
+  var btnBox = $(".btn-box");
+
   //===============================
   //========Настройки QUIZ=========
   //===============================
   //Начальный размер скидки
-	var title="Благодарим за ваши ответы!";
+  var title = "Благодарим за ваши ответы!";
   var days = 31;
   var discript = "на любую вашу сделку";
   //Количество всех вопросов
   //
   //===============================
 
-	ChangeValuesTemplate();
+  ChangeValuesTemplate();
+
   function changeSuffix(value) {
     if (value == 1) {
       return "день";
@@ -36,12 +38,13 @@ $(function() {
       return "дней";
     }
   }
-	function ChangeValuesTemplate(){
-		templateTitleBox.text(title);
-	  discriptBox.text(discript);
-		daysBox.text(days);
-	  suffixBox.text(changeSuffix(days));
-	}
+
+  function ChangeValuesTemplate() {
+    templateTitleBox.text(title);
+    discriptBox.text(discript);
+    daysBox.text(days);
+    suffixBox.text(changeSuffix(days));
+  }
 
   var endTemplate = {
     title: "Благодарим за ответы!",
@@ -89,10 +92,10 @@ $(function() {
         "Ответ №4 вопроса 2"
       ],
       img: [
-        "img/typeProduct/question-2/1.jpg",
-        "img/typeProduct/question-2/2.jpg",
-        "img/typeProduct/question-2/3.jpg",
-        "img/typeProduct/question-2/4.jpg"
+        "",
+        "",
+        "",
+        ""
       ],
       type: "grid"
     },
@@ -134,14 +137,19 @@ $(function() {
 
   //Generated Answer-Box for Quiz
   var currentQuestion = 0;
-	var startQuestion=1;
+  var startQuestion = 1;
+
+  function ListGenerate() {
+    var list = "<div id='list-answer'><ul class='list-group'></ul></div>";
+    answerBox.append(list);
+  }
 
   function Generated() {
     var answersLength = question[currentQuestion].answers.length;
     var qstnCount = question.length;
     var title = question[currentQuestion].title;
-    console.log(answersLength);
     var choice;
+
     for (var i = 0; i < answersLength; i++) {
       var sizeCol = 0;
       switch (counter()) {
@@ -165,23 +173,32 @@ $(function() {
           break;
       }
       choice = question[currentQuestion].answers[i];
-      console.log(choice);
       img = question[currentQuestion].img[i];
       qstnTitle.text(title);
-      var bodyAnswer = "<div class='col-md-" + sizeCol + " text-center'>" +
-        choice +
-        "<br><img class='img-responsive rounded' src=" + img + "><br>" +
-        "<input class='radio-btn' name='answer' value='question" + i + "' type='radio'>" +
-        "</div>";
-      answerBox.append(bodyAnswer);
+
+
+      if (img === "") {
+        var bodyAnswer = "<li class='list-group-item'><input class='radio-btn' name='answer' value='question" + i + "' type='radio'>"+choice+
+          "</li>";
+        $(".list-group").append(bodyAnswer);
+      } else {
+        var bodyAnswerFull = "<div class='col-md-" + sizeCol + " text-center animated fadeIn'>" +
+          choice +
+          "<br><img class='img-responsive rounded' src=" + img + "><br>" +
+          "<input class='radio-btn' name='answer' value='question" + i + "' type='radio'>" +
+          "</div>";
+        answerBox.append(bodyAnswerFull);
+      }
+
+
     }
   }
   Generated();
 
   function Template() {
-		$.each(endTemplate.advantages, function(key, value){
-			listGroupBox.append("<li class='list-group-item'><i class='"+value.icon+"'></i> "+value.title+"</li>");
-		});
+    $.each(endTemplate.advantages, function(key, value) {
+      listAdvantageBox.append("<li class='list-group-item animated fadeIn'><i class='" + value.icon + "'></i> " + value.title + "</li>");
+    });
 
   }
   Template();
@@ -195,43 +212,58 @@ $(function() {
     return count;
   }
 
-	qstnStartBox.text(startQuestion);
-	qstnEndBox.text(counter());
+  qstnStartBox.text(startQuestion);
+  qstnEndBox.text(counter());
 
 
   //Revome Question
   function Clear() {
-
     qstnTitle.empty();
     answerBox.empty();
   }
   //Add values in progress-bar
-  var step = Math.round((100 / question.length));
-  var valBar=step;
-	progressBar.css("width",valBar+"%");
+
+  var step = Math.round((100 / question.length - 1));
+  var valBar = step;
+
+  progressBar.css("width", valBar + "%");
 
   function Up() {
-		valBar+=step;
-    progressBar.css("width",valBar+"%");
-		progressBar.attr("aria-valuenow", valBar);
-		$(".progress-bar").text(valBar+"%");
+    valBar += step;
+    progressBar.css("width", valBar + "%");
+    progressBar.attr("aria-valuenow", valBar);
+    $(".progress-bar").text(valBar + "%");
 
-		//change question-start
-		startQuestion+=1;
-		qstnStartBox.text(startQuestion);
+    //change question-start
+    startQuestion += 1;
+    qstnStartBox.text(startQuestion);
+  }
+
+  function progressBarFull() {
+    progressBar.css("width", 100 + "%");
+    progressBar.attr("aria-valuenow", 100);
+    $(".progress-bar").text("100%");
   }
 
   //Handler event click on ipnut
-  $(document).on("click",".radio-btn",function() {
-			$(".btn-box").show();
+  $(document).on("click", ".radio-btn", function() {
+		$(".btn-box").show();
   });
-  $(document).on("click",".btn-next",
+  $(document).on("click", ".btn-next",
     function() {
-			$(".btn-box").hide();
-      Clear();
       currentQuestion++;
-      Generated();
-			Up();
+      if (question.length > currentQuestion) {
+        $(".btn-box").hide();
+        Clear();
+        Up();
+        ListGenerate();
+        Generated();
+
+      } else {
+        progressBarFull();
+      }
+
+
     }
   );
 });
